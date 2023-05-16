c2409550008f03d36ac389350849d9e8f5613795
Amal-web <amalsivadas08@gmail.com>
2023-05-16 20:31:31 +0530

    video added

--- src/Pages/Eventspage/Eve.js
+++ src/Pages/Eventspage/Eve.js
@@ -6,6 +6,17 @@ const Event = () => {
   return (
     <div className={classes.event}>
       <div className={classes.eventPara}>
+        <div style={{textAlign:"center"}}>
+          <iframe
+            width="900"
+            height="500"
+            src="https://www.youtube.com/embed/8TJHMehA47o?autoplay=1&mute=1"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
         <p>
           Student events are student-focused extracurricular clubs and programs
           offered at a college or university. Student activities are generally
